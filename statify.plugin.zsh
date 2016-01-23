1a6552e74e8eee36218d2eab650eb7b7c3514f6b
Vlad Marinov <vladmrnv@gmail.com>
2016-01-22 16:11:56 -0800

    Added Median func

--- statify.plugin.zsh
+++ statify.plugin.zsh
@@ -1,4 +1,21 @@
+# This is the basic calculator function
+# $ calc 2 + 1 = 3
+# $ calc 2 - 1 = 1
+# $ calc 5 / 2 = 2.500'
+# $ calc 4 % 2 = 2
+# $ calc "5 * 10" = 50 (You need the " " because * is the wildcard in UNIX )
+# Essentially anything that 'bc' can do, so can you, however it must be like
+# the example above, surrounded in " ", ex: $ calc "sqrt(16)" >>> 4
 function bc_convert {
-  echo "$@" | bc -l
+  echo "$@" | bc
 }
-alias calc="bc_convert" 
+
+# Median function
+# Probably best to keep with Odd numbers
+# $ median 5 7 3 4 9
+# >>> 5
+function median {
+  echo  $@ | xargs -n1 | sort -n | sed -n $((($#+1)/2))p
+}
+
+alias calc="bc_convert"
